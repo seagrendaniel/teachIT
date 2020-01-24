173a7267fef3ba101d3a56d66880cf304fbad82d
blackdanamite <seagrendaniel562@gmail.com>
2020-01-24 13:57:25 -0800

    initialize app and style

--- App.js
+++ App.js
@@ -3,8 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    <View style={styles.container}> 
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is just an initialization of the app</Text>
+    </View>
     </View>
   );
 }
@@ -16,4 +18,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  letterDisplay: {
+    fontSize: 30
+  }
 });
